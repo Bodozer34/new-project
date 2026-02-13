2161d054d4c17c942c2d3b5cce617520195221ff
maythinn <developer.maythinn@gmail.com>
2026-02-13 18:17:46 +0630

    feat: add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TIimeTracking from './Components/TimeTracking'
-import TimeTracking from './Components/TimeTracking'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TIimeTracking from "./Components/TimeTracking";
+import TimeTracking from "./Components/TimeTracking";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-   <TimeTracking/>
-      
+      <h1>Hello MT</h1>
+      <TimeTracking />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
